57b3647ff33d67576c825eb0b80321c0e89b86eb
Austin Wagner <austin.e.wagner@gmail.com>
2018-04-04 15:40:37 -0700

    quiz structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+const quiz = {
+    q1: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q2: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q3: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q4: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q5: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q6: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q7: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q8: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q9: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    },
+    q10: {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctAnswer: ''
+    }
+}
